08a05983fb282feed5898e79514bedf0d3b9dd12
shifa-Alam <shifa7446@gmail.com>
2022-12-31 22:54:31 +0600

    Add project files.

---
+++ 3Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿IList<IList<int>> ThreeSum(int[] nums)
+{
+    List<IList<int>> result = new List<IList<int>>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            for (int k = j + 1; k < nums.Length; k++)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    List<int> list = new List<int>();
+                    list.Add(nums[i]);
+                    list.Add(nums[j]);
+                    list.Add(nums[k]);
+
+                    list.Sort();
+
+                    result.Add(list);
+                }
+            }
+        }
+    }
+
+   
+    result = result.Distinct().ToList();
+
+    return result;
+}
+
+int[] nums = { -1, 0, 1, 2, -1, -4 };
+ThreeSum(nums);
---
+++ TwoSum/Program.cs
@@ -0,0 +1,25 @@
+﻿
+ int[] TwoSum(int[] nums, int target)
+{
+	int[] result = new int[2];
+
+	for (int i = 0; i < nums.Length; i++)
+	{
+		for (int j = i+1; j < nums.Length; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				result[0]=i;
+				result[1]=j;
+				break;
+			}
+		}
+	}
+	return result;
+}
+//int[] nums = { 2, 7, 11, 15 };
+//TwoSum(nums, 9);
+//int[] nums = { 3, 2, 4 };
+//TwoSum(nums, 6);
+int[] nums = { 3, 3 };
+TwoSum(nums, 6);
