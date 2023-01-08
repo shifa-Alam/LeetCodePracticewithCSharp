2eeea44a99e481b85eff815fbffc70c093a7f9cf
shifa-Alam <shifa7446@gmail.com>
2023-01-08 18:08:11 +0600

    updated

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,8 @@
+﻿int LengthOfLastWord(string s)
+{
+    string[] words = s.Trim().Split(' ');
+    int length= words[words.Length-1].Length;
+    return length;
+}
+string s = "   fly me   to   the moon  ";
+LengthOfLastWord(s);
---
+++ Search Insert Position/Program.cs
@@ -0,0 +1,24 @@
+﻿int SearchInsert(int[] nums, int target)
+{
+    int start = 0;
+    int end = nums.Length - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if(nums[mid] == target)
+        {
+            return mid;
+        }else if (target > nums[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return end + 1;
+}
+int[] nums = { 1, 3, 5, 6 };
+int target = 2;
+SearchInsert(nums, target);
