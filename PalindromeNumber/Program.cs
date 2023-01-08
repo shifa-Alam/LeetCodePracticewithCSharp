e8be83002a8fce63cfd76d0374bab2fa4623b63a
shifa-Alam <shifa7446@gmail.com>
2023-01-08 13:35:23 +0600

    pelondrome done

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿
+bool CheckPalindrome(int x)
+{
+    if (x < 0)
+    {
+        return false;
+    }
+     string stringNum = x.ToString();
+
+    string  reverseString = string.Empty;
+    for (int i = stringNum.Length-1; i >= 0; i--)
+    {
+        reverseString = reverseString +stringNum[i].ToString();
+    }
+    if (reverseString.Equals(stringNum)) return true;
+
+    return false;
+
+}
+
+Console.WriteLine(CheckPalindrome(121));
