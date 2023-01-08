c202dc3cca23751ff776994403f480500f97248e
shifa-Alam <shifa7446@gmail.com>
2023-01-08 21:42:36 +0600

    c

---
+++ Plus One/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] PlusOne(int[] digits)
+{
+    int n = digits.Length;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (digits[i] < 9)
+        {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    int[] newNumber = new int[n + 1];
+    newNumber[0] = 1;
+    return newNumber;
+}
+
+int[] digits = {9,9};
+PlusOne(digits);
